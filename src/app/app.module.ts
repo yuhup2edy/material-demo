2295a7605caeea16838ef7e4958b534a8f1e93d5
Sriram Venkataramani <yuhup2edy@FLTRGTRPLAP005.nrtss.com>
2020-05-04 15:18:34 -0400

    Fixed material import; check module.ts for npm instruction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule} from '@angular/material';
+
+import { MatButtonModule} from '@angular/material/button';
+//import { MatMenuModule } from '@angular/material/menu';
+//import { MatToolbarModule } from '@angular/material/toolbar';
+//import { MatIconModule } from '@angular/material/icon';
+//import { MatCardModule } from '@angular/material/card';
+ 
 
 @NgModule({
   declarations: [
@@ -21,3 +27,7 @@ import { MatButtonModule} from '@angular/material';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// npm install --save angular/material2-builds angular/cdk-builds <== this is to overcome the deprecations
+// material.angular.io for all of the information related the package
