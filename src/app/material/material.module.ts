70d34580663ce11588960b08bd44d68f19f60785
Sriram V <xsriram@gmail.com>
2020-05-05 00:54:51 -0400

    Added Badge

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatBadgeModule } from '@angular/material/badge';
 
 //import { MatMenuModule } from '@angular/material/menu';
 //import { MatToolbarModule } from '@angular/material/toolbar';
 //import { MatCardModule } from '@angular/material/card';
 
-const MaterialComponents = [ MatButtonModule, MatButtonToggleModule, MatIconModule ] 
+const MaterialComponents = [ MatButtonModule, MatButtonToggleModule, MatIconModule, MatBadgeModule ] 
 
 @NgModule({
   imports: [MaterialComponents],
