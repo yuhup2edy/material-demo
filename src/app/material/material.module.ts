ee3379681ab37fa13673dbca5f53cdc81716ebb5
Sriram V <xsriram@gmail.com>
2020-05-06 01:12:29 -0400

    added menu

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule }              from '@angular/material/button';
-import {MatButtonToggleModule}          from '@angular/material/button-toggle'; 
-import {MatIconModule}                  from '@angular/material/icon';
-import { MatBadgeModule}                from '@angular/material/badge';         
-import {MatProgressSpinnerModule}       from '@angular/material/progress-spinner';
-import {MatProgressBarModule}           from '@angular/material/progress-bar';
-import {MatToolbarModule }             from '@angular/material/toolbar';
-import  { MatSidenavModule }             from '@angular/material/sidenav';
+import { MatButtonModule }                from '@angular/material/button';
+import { MatButtonToggleModule }          from '@angular/material/button-toggle'; 
+import { MatIconModule }                  from '@angular/material/icon';
+import { MatBadgeModule }                 from '@angular/material/badge';         
+import { MatProgressSpinnerModule }       from '@angular/material/progress-spinner';
+import { MatProgressBarModule }           from '@angular/material/progress-bar';
+import { MatToolbarModule }               from '@angular/material/toolbar';
+import { MatSidenavModule }               from '@angular/material/sidenav';
+import { MatMenuModule }                  from '@angular/material/menu';  
 
 //import { MatMenuModule } from '@angular/material/menu';
 //import { MatToolbarModule } from '@angular/material/toolbar';
@@ -20,7 +21,8 @@ const Material = [ MatButtonModule,
                    MatProgressSpinnerModule,
                    MatProgressBarModule,
                    MatToolbarModule,
-                   MatSidenavModule ] 
+                   MatSidenavModule,
+                   MatMenuModule ] 
 
 @NgModule({
   imports: [Material],
