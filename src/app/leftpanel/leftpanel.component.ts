f081f637da7a245b23b070e8b69f5e0269c7841b
Sriram V <xsriram@gmail.com>
2020-05-08 20:50:02 -0400

    Added left and right panel for testing

---
+++ src/app/leftpanel/leftpanel.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leftpanel',
+  template: `
+  <input matInput required>
+  `
+,
+    styles: []
+})
+export class LeftpanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/rightpanel/rightpanel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rightpanel',
+  template: `
+    <p>
+      rightpanel works!
+    </p>
+  `,
+  styles: []
+})
+export class RightpanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
