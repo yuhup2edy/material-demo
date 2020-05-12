8593d7e654e2e9abc90135927e099523599b853f
Sriram V <xsriram@gmail.com>
2020-05-12 00:33:33 -0400

    Validations completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core'; //view child is for table sorting 
-import { FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core'; 
 import { Observable} from 'rxjs';
 import { map,startWith } from 'rxjs/operators';
@@ -7,7 +7,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator} from '@angular/material/paginator';
-
+import { MatDatepickerInputEvent} from '@angular/material/datepicker';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -21,16 +21,32 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit, AfterViewInit
 {
 
   currDate = new Date();
   minDate  = new Date();
+  skipDay : number;
+  dateEvents: string[] = [];
 
+  
   ngOnInit()
   {
-    //minDate returns today + 3 days as default selector for need by 
+    //minDate returns today + 3 days as default selector for need by ; adjust for sat  & sun
+    
     this.minDate = new Date(this.currDate.getTime() + 3 * (1000 * 60 * 60 * 24));
+    this.skipDay = this.minDate.getDay();
+    
+    if (this.skipDay === 0)
+    {
+      this.minDate = new Date(this.currDate.getTime() + 4 * (1000 * 60 * 60 * 24));
+    }
+    else if (this.skipDay === 6)
+    {
+      this.minDate = new Date(this.currDate.getTime() + 5 * (1000 * 60 * 60 * 24));
+    } 
+
   }
 
   ngAfterViewInit()
@@ -46,14 +62,37 @@ export class AppComponent implements OnInit, AfterViewInit
   }
   // all form definitions & validations begin here
   
-  rarrequesterId = new FormControl('', [
+  rarRequesterId = new FormControl('', [
     Validators.required,
     Validators.minLength(6),
     Validators.pattern('^[a-zA-Z]+$')
   ]);
 
-  // all form definitions & validations end here
+  rarApplication = new FormControl('', [
+    Validators.required,
+  ]);
+  
+  rarRole = new FormControl('', [
+    Validators.required,
+  ]);
+  
+  rarApprover = new FormControl('', [
+    Validators.required,
+  ]);
+
+  rarNeedByDate = new FormControl('', [
+    Validators.required,
+  ]);
+
+  rarUsersList = new FormControl('', [
+    Validators.required,
+  ]);
+
+  // calling the global validator
+  
   validator = new MyErrorStateMatcher();
+  
+ // all form definitions & validations end here
 
 } // close of the AppComponent Class
 
